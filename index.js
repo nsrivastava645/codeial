9eb34be6f117750508afe951e008b49724303e90
nsrivastava645 <nsrivastava645@gmail.com>
2020-05-25 18:38:27 +0530

    Add view-engine and install ejs

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//any app will use the routes index file then index of routes filewill use middleware to route to its neighbours using router.use method
+
 //use the express router 
 app.use('/', require('./routes'));//by default fetches routes/index
 
+//setting the view engine
+app.set('view-engine', 'ejs');
+
+//set views directory
+app.set('views','./views');
 
 //add listener on this port 
 app.listen(port,/*callback*/function(err){
