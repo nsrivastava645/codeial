dafceadd6ad043979f700cbe2ecde3839a6147e9
nsrivastava645 <nsrivastava645@gmail.com>
2020-05-25 21:20:08 +0530

    Add static files directory and linked it to express.static

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const expressLayouts = require('express-ejs-layouts');
 //add layout to your index.js
 app.use(expressLayouts);
 
+//for css styling of each view file and scripts
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+//and add <%- style %> in the layout ejs file
 
+//adding static files
+app.use(express.static('./assets'));
 //any app will use the routes index file then index of routes filewill use middleware to route to its neighbours using router.use method
 
 
