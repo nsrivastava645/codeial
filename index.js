a3961374dc8e76940b22860125b10e9f00b31abe
nsrivastava645 <nsrivastava645@gmail.com>
2020-05-26 05:44:33 +0530

    Add  mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db  = require('./config/mongoose');
+
+
+
 
 //add layout to your index.js
 app.use(expressLayouts);
