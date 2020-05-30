88895a7962c59f4430c11a05934450d9c94a5240
nsrivastava645 <nsrivastava645@gmail.com>
2020-05-30 06:51:08 +0530

    add node sass middleware and port to sass for styling

--- index.js
+++ index.js
@@ -13,10 +13,18 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
 
+//sass
+const sassMiddleware = require('node-sass-middleware');
 
 
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
 
-
+}));
 //use the cookie parser
 app.use(cookieParser());
 
